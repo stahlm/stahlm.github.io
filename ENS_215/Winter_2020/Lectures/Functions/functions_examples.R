2ce01a5d6508a60b88882b38d74bfadf5de090e1
Mason Stahl <42699757+stahlm@users.noreply.github.com>
2020-03-11 09:43:55 -0400

    Update

---
+++ ENS_215/Winter_2020/Lectures/Functions/functions_examples.R
@@ -0,0 +1,18 @@
+
+cube_it <- function(cube_me){
+  # Function computes the cube of the input
+  # inputs: cube_me is the value to cube
+  # output: the cube of cube_me
+  
+  cube_me^3 # take the cube of the input cube_me
+}
+
+
+pow_it <- function(base_val, exp_val){
+  # Raises an input value to a specified power
+  # inputs: base_val is the value to be raised to a power; exp_val is the exponent value
+  # output: base_val raised to the exp_val
+  
+  base_val^exp_val
+}
+
