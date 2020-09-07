8498f4e8e985c0999d439d062d6ee22a08f960cf
Stahl <stahlm@union.edu>
2020-09-06 21:33:46 -0400

    update

---
+++ ENS_GEO_210/functions/get_usgs_elevation_data.R
@@ -0,0 +1,67 @@
+
+
+#' Download USGS elevation data from the ArcGIS REST API.
+#'
+#' @param bbox bounding box coordinates (list of 2 points with long/lat values)
+#' @param size image size as a string with format "<width>,<height>"
+#' @param file file path to save to. Default is NULL, which will create a temp file.
+#' @param sr_bbox Spatial Reference code for bounding box
+#' @param sr_image Spatial Reference code for elevation image
+#' 
+#' @details This function uses the ArcGIS REST API, specifically the 
+#' exportImage task. You can find links below to a web UI for this
+#' rest endpoint and API documentation.
+#' 
+#' Web UI: https://elevation.nationalmap.gov/arcgis/rest/services/3DEPElevation/ImageServer/exportImage
+#' API docs: https://developers.arcgis.com/rest/services-reference/export-image.htm
+#'
+#' @return file path for downloaded elevation .tif file. This can be read with
+#' \code{read_elevation_file()}.
+#'
+#' @examples
+#' bbox <- list(
+#'   p1 = list(long = -122.522, lat = 37.707),
+#'   p2 = list(long = -122.354, lat = 37.84)
+#' )
+#' image_size <- define_image_size(bbox, 600)
+#' elev_file <- get_usgs_elevation_data(bbox, size = image_size$size)
+#' 
+get_usgs_elevation_data <- function(bbox, size = "400,400", file = NULL, 
+                                    sr_bbox = 4326, sr_image = 4326) {
+  require(httr)
+  
+  # TODO - validate inputs
+  
+  url <- parse_url("https://elevation.nationalmap.gov/arcgis/rest/services/3DEPElevation/ImageServer/exportImage")
+  res <- GET(
+    url, 
+    query = list(
+      bbox = paste(bbox$p1$long, bbox$p1$lat, bbox$p2$long, bbox$p2$lat,
+                   sep = ","),
+      bboxSR = sr_bbox,
+      imageSR = sr_image,
+      size = size,
+      format = "tiff",
+      pixelType = "F32",
+      noDataInterpretation = "esriNoDataMatchAny",
+      interpolation = "+RSP_BilinearInterpolation",
+      f = "json"
+    )
+  )
+  
+  if (status_code(res) == 200) {
+    body <- content(res, type = "application/json")
+    # TODO - check that bbox values are correct
+    # message(jsonlite::toJSON(body, auto_unbox = TRUE, pretty = TRUE))
+    
+    img_res <- GET(body$href)
+    img_bin <- content(img_res, "raw")
+    if (is.null(file)) 
+      file <- tempfile("elev_matrix", fileext = ".tif")
+    writeBin(img_bin, file)
+    message(paste("image saved to file:", file))
+  } else {
+    warning(res)
+  }
+  invisible(file)
+}
